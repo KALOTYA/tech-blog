af7ee138e8326fac07f9759454941ff422fad6c6
Ajay Kaloty <kalotya99@gmail.com>
2024-04-30 23:12:09 -0700

    Added a comments model

---
+++ models/Comment.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    blogPostId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'blog_posts',
+        key: 'id',
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'comment',
+    tableName: 'comments',
+  }
+);
+
+module.exports = Comment;
